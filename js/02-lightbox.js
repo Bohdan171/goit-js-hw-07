6aece85b7d09f10aa1ee2f37461e37a136cc8147
Bohdan Onyshko <bogdan_onishko@ukr.net>
2022-06-03 22:39:57 +0300

    2 задание

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
+galleryItems.forEach(picture => {
+    
+    const linkEL = document.createElement('a');
+    linkEL.classList.add('gallery__item');
+  linkEL.href = picture.original;
+ 
+
+    const imageEL = document.createElement('img');
+    imageEL.classList.add('gallery__image');
+    imageEL.src = picture.preview;
+    imageEL.alt = picture.description;
+
+    linkEL.append(imageEL);
+
+    galleryEl.append(linkEL)
+
+})
+
+    
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
 console.log(galleryItems);
